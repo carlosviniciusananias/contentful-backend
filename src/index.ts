87223b671178d336e98c8787033e4debf1828def
Carlos Vinicius <carlos.oliveira69@fatec.sp.gov.br>
2022-09-19 20:41:37 -0300

    fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express from "express";
-// import cors from "cors";
 import route from "./routes/route";
 
 const PORT = process.env.PORT || 4000;
@@ -10,10 +9,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("hello service");
+  res.send("hello, welcome to my service");
 });
 
 app.use("/api", route);
+
+app.use((req, res) => {
+  res.status(404);
+});
 
 app.listen(PORT, () => {
   console.log("server start");
