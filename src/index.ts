0f96b01cc9554c0aa1454bc4fd1566288564e225
Carlos Vinicius <carlos.oliveira69@fatec.sp.gov.br>
2022-09-19 22:21:41 -0300

    added new settings

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import express from "express";
-import route from "./routes/route";
+import express, { Express, Request, Response } from 'express';
+import router from "./routes/route";
 
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (req: Request, res: Response) => {
   res.send("hello, welcome to my service");
 });
 
-app.use("/api", route);
+app.use("/api", router);
 
 app.use((req, res) => {
   res.status(404);
--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,12 +1,12 @@
-import express from "express";
+import { Router } from "express";
 import getProducts from "../services/products";
 
-const route = express.Router();
+const router = Router();
 
-route.get("/products", async (req, res) => {
+router.get("/products", async (req, res) => {
   const data = await getProducts();
 
   res.send(data);
 });
 
-export default route;
+export default router;
