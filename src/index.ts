9381829d1ceca7dbb59cb0083d971469623148af
Carlos Vinicius <carlos.oliveira69@fatec.sp.gov.br>
2022-09-19 22:26:39 -0300

    fix tsconfig

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 import router from "./routes/route";
 
 const PORT = process.env.PORT || 5000;
