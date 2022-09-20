0f82dc074ca81f7506903591bcf9e27282456719
Carlos Vinicius <carlos.oliveira69@fatec.sp.gov.br>
2022-09-19 23:18:28 -0300

    fix routes

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const router = require("./src/routes/route");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("hello, welcome to my service");
-});
 
 app.use("/api", router);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,10 @@ const { getProducts } = require("../services/products");
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  res.send("hello, welcome to my service");
+});
+
 router.get("/products", async (req, res) => {
   const data = await getProducts();
 
