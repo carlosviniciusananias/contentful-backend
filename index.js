54d7e46acf6b0079927275431e4b1da86371751a
Carlos Vinicius <carlos.oliveira69@fatec.sp.gov.br>
2022-09-19 22:56:10 -0300

    fix index

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 app.get("/", (req, res) => {
   res.send("hello, welcome to my service");
 });
 
 app.use("/api", router);
-
-app.use((req, res) => {
-  res.status(404);
-});
 
 app.listen(PORT, () => {
   console.log("server start");
